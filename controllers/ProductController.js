034fbc605bed6a5dada9797b889ceaf31e237f07
giahao1411 <trangiahao498@gmail.com>
2024-11-08 01:16:09 +0700

    second push

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -2,11 +2,35 @@ const Product = require("../models/Product");
 
 const homePage = async (req, res) => {
     try {
-        const products = await Product.find();
+        products = await Product.find();
         return res.status(200).json(products);
     } catch (err) {
         return res.status(500).json({ message: err.message });
     }
 };
 
-module.exports = { homePage };
+const addProduct = async (req, res) => {
+    try {
+        const { name, description } = req.body;
+
+        const newProd = await Product.create({
+            name,
+            description,
+        });
+
+        return res.status(200).json({ message: "Product added" });
+    } catch (err) {
+        return res.status(500).json({ message: err.message });
+    }
+};
+
+const deleteProduct = async (req, res) => {
+    try {
+        await Product.deleteMany();
+        return res.status(200).json({ message: "Products deleted" });
+    } catch (err) {
+        return res.status(500).json({ message: err.message });
+    }
+};
+
+module.exports = { homePage, addProduct, deleteProduct };
--- routes/ProductRouter.js
+++ routes/ProductRouter.js
@@ -4,4 +4,8 @@ const ProductController = require("../controllers/ProductController");
 
 router.get("/", ProductController.homePage);
 
+router.post("/", ProductController.addProduct);
+
+router.delete("/", ProductController.deleteProduct);
+
 module.exports = router;
